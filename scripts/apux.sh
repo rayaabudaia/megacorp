3d608c4c832628f2b2fcbc42e947ef0b87720832
rayaabudaia <raya662003@gmail.com>
2026-03-19 15:26:07 +0200

    L: fix apux.sh to replace ' sh' with ' apux'

--- scripts/apux.sh
+++ scripts/apux.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-output=$(sh "$@" 2>&1)
-echo "$output" | sed 's/bash/apux/g'
+#!/bin/bash
+
+# This script runs a given command and replaces any instance of "sh" in the output with "apux"
+# It's a feature that our customers love!
+
+# Run the command and capture output
+output=$("$@" 2>&1)
+
+# Replace "sh" with "apux" in the output
+echo "$output" | sed 's/ sh/ apux/g'
